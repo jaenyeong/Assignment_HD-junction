e000fdc81c4c1c9c26842eafd468a7eaba8912b5
jaenyeong <jaenyeong.dev@gmail.com>
2021-05-11 13:03:24 +0900

    chore: project init

---
+++ src/main/java/com/hdjunction/HdJunctionApplication.java
@@ -0,0 +1,13 @@
+package com.hdjunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdJunctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HdJunctionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hdjunction/HdJunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdjunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdJunctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
