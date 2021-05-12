ace9d91617ffa470509072be1082d6b2d98b0413
jaenyeong <jaenyeong.dev@gmail.com>
2021-05-12 23:34:38 +0900

    feat(patient): add patient entity

---
+++ src/main/java/com/hdjunction/entity/Patient.java
@@ -0,0 +1,38 @@
+package com.hdjunction.entity;
+
+import com.hdjunction.common.CodeGroup;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PATIENT_ID")
+    private Long id;
+
+    private Long hospitalId;
+
+    private String name;
+
+    private String registrationId;
+
+    private CodeGroup.Code sexCode;
+
+    private String dateOfBirth;
+
+    private String phoneNo;
+
+    public Patient(final Long hospitalId, final String name, final String registrationId, final CodeGroup.Code sexCode,
+                   final String dateOfBirth, final String phoneNo) {
+        this.hospitalId = hospitalId;
+        this.name = name;
+        this.registrationId = registrationId;
+        this.sexCode = sexCode;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNo = phoneNo;
+    }
+}
