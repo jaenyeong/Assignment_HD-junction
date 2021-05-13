3b2ab476abb699daf8386336495768e2ca4f69e0
jaenyeong <jaenyeong.dev@gmail.com>
2021-05-13 15:21:15 +0900

    refactor(patient): refactor patient dto field, constructor

--- src/main/java/com/hdjunction/dto/PatientRequest.java
+++ src/main/java/com/hdjunction/dto/PatientRequest.java
@@ -1,12 +1,14 @@
 package com.hdjunction.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class PatientRequest {
     @NotNull(message = "병원 정보를 입력하세요.")
--- src/main/java/com/hdjunction/dto/PatientResponse.java
+++ src/main/java/com/hdjunction/dto/PatientResponse.java
@@ -1,12 +1,24 @@
 package com.hdjunction.dto;
 
+import com.hdjunction.entity.Patient;
 import lombok.Data;
 
 @Data
 public class PatientResponse {
+    private String id;
     private String name;
+    private String registrationId;
     private String sex;
     private String dateOfBirth;
     private String phoneNo;
     private String recentVisit;
+
+    public PatientResponse(final Patient patient) {
+        this.id = Long.toString(patient.getId());
+        this.name = patient.getName();
+        this.registrationId = patient.getRegistrationId();
+        this.sex = patient.getSexCode();
+        this.dateOfBirth = patient.getDateOfBirth();
+        this.phoneNo = patient.getPhoneNo();
+    }
 }
