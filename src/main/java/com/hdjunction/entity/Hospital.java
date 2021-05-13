9cbcdcbc5c904a176a39effaa51659d3da4ce3f6
jaenyeong <jaenyeong.dev@gmail.com>
2021-05-13 10:49:01 +0900

    refactor(hospital): update field attribute to not null from nullable

--- src/main/java/com/hdjunction/entity/Hospital.java
+++ src/main/java/com/hdjunction/entity/Hospital.java
@@ -4,6 +4,7 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -13,10 +14,13 @@ public class Hospital extends BaseTimeEntity {
     @Column(name = "HOSPITAL_ID")
     private Long id;
 
+    @NotNull
     private String name;
 
+    @NotNull
     private String sanatoriumId;
 
+    @NotNull
     private String directorName;
 
     public Hospital(final String name, final String sanatoriumId, final String directorName) {
