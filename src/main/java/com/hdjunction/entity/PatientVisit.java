18375a30e679c4a77a7a700a52dc690b6406f0fc
jaenyeong <jaenyeong.dev@gmail.com>
2021-05-13 09:46:19 +0900

    feat(patient visit): add patient visit entity

---
+++ src/main/java/com/hdjunction/entity/PatientVisit.java
@@ -0,0 +1,24 @@
+package com.hdjunction.entity;
+
+import com.hdjunction.common.CodeGroup;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class PatientVisit extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "HOSPITAL_ID")
+    private Hospital hospital;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "PATIENT_ID")
+    private Patient patient;
+
+    @NotNull
+    private CodeGroup.Code visitCode;
+}
